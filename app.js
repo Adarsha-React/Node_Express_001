ee49faeca336d1895cd9a801cbfb14d821b4c552
= <=>
2023-09-15 21:15:40 +0530

    Got Express.js and Exectuted simple get method

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
-const superVillains = require("supervillains");
+const express = require("express");
+const app = express();
 
-console.log(superVillains.random());
+app.get("/", function (req, res) {
+  res.send("Hello World!!");
+  console.log("Server is up and running");
+});
+
+app.get("/about/:id", (req, res) => {
+  res.send("about page for the user id: " + req.params.id);
+});
+
+app.listen(3000);
